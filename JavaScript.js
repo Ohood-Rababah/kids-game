71afabba287755bff69e7e9f073cbd7b779ba728
Ohood Rababah <ohood1921@gmail.com>
2024-11-16 19:31:58 +0300

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,19 @@
+﻿function start() {
+
+    const name = document.getElementById("nameinput").value;
+    const age = document.getElementById("ageinput").value;
+
+    if (name && age) {
+        alert(`Hello, ${name}! Get ready to play!`);
+        location.href = "testexam.html"
+    }
+    else {
+        alert('Please enter your name and age to continue!');
+    }
+    
+}
+//end page1
+
+const backgroundMusic = new Audio('background.mp3'); // موسيقى خلفية
+backgroundMusic.loop = true; // تشغيل الموسيقى في حلقة
+
